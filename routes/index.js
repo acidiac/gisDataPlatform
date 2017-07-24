0d2e436a50b52ddd6adbb5905b0a94841aae8f42
Amit Chanchal <amit.chanchal@hotmail.com>
2017-07-24 12:12:56 +0530

    First routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    res.render('index', { title: 'Express' });
+});
+
+router.get('/addData', (req, res, next) => {
+    // The router function handle the rendering of the main data adding webpage
+    res.render('addData', { title: 'add Data' });
 });
 
 module.exports = router;
